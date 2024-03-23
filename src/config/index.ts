56c3717b7a2dca5510fac47112ddeba3372d231e
Jonny David Lezcano Algarin <jlezcano@mideacarrier.com>
2024-03-23 18:05:05 -0300

    fix: add to use new url to download .zip files with RUC

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 
 dotenv.config()
 
-export const URL_ROOT = `https://www.set.gov.py/documents/20123/517070/ruc`
+export const URL_ROOT = `https://www.dnit.gov.py/documents/20123/662360/ruc`
 export const CRON_SCHEDULE = process.env.CRON_SCHEDULE || '0 */1 * * * *'
 export const DATABASE_URL = process.env.DATABASE_URL || 'file:../data/rucpy.db'
 export const TIMEZONE = process.env.TIMEZONE || 'America/Asuncion'
